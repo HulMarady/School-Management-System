85f2c43b9a1356db985001b1107e173bdf6dac10
HulMarady <marady.hul@student.cadt.edu.kh>
2026-03-16 16:33:22 +0700

    Add University model with properties Id, Name, and Location

---
+++ Models/University.cs
@@ -0,0 +1,8 @@
+﻿namespace School_Management_System.Models;
+
+public class University
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Location { get; set; }
+}
