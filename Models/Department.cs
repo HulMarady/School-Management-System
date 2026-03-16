b8c49361df0581cad3009a1017f89775e118acee
HulMarady <marady.hul@student.cadt.edu.kh>
2026-03-16 16:39:11 +0700

    Add Department model with properties Id, Name, UniversityId, and University

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace School_Management_System.Models;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int UniversityId { get; set; }
+    public University University { get; set; }
+}
