c999731c01f5421206b930acc2041c2c29502cc4
HulMarady1 <hulmarady1@gmail.com>
2026-03-17 08:26:33 +0700

    Add Validation Rule On Properties Of University Model

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,14 @@
-﻿namespace School_Management_System.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace School_Management_System.Models;
 
 public class Department
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "{0} is required")]
+    [StringLength(120, MinimumLength = 6, ErrorMessage = "{0} must be between {2} and {1} characters")]
     public string Name { get; set; }
     public int UniversityId { get; set; }
     public University University { get; set; }
