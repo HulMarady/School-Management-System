5201ecf0e2613b19723159aae348f1dd2891fc9c
HulMarady1 <hulmarady1@gmail.com>
2026-03-18 15:05:34 +0700

    Add User Model with Properties for Authentication and Role Management

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School_Management_System.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    public string Username { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+    public string Role { get; set; } // e.g., "Admin", "Student", "Teacher"
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
