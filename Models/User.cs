d75cbbff2d4caf9677d6c08257229982b4611a85
HulMarady1 <hulmarady1@gmail.com>
2026-03-27 10:39:06 +0700

    Refactor User Model by Adding First Name, Last Name, and Role

--- Models/User.cs
+++ Models/User.cs
@@ -5,15 +5,33 @@ namespace School_Management_System.Models;
 public class User
 {
     public int Id { get; set; }
+
     [Required]
+    [Display(Name = "First Name")]
+    [StringLength(50, MinimumLength = 6)]
+    [RegularExpression("^[a-zA-Z ]+$")]
+    public string FirstName { get; set; }
+    [Required]
+    [Display(Name = "Last Name")]
+    [StringLength(50, MinimumLength = 6)]
+    [RegularExpression("^[a-zA-Z ]+$")]
+    public string LastName { get; set; }
+
+    [Required]
+    [Display(Name = "Username")]
+    [StringLength(100, MinimumLength = 6)]
+    [RegularExpression(@"^[a-zA-Z0-9_ ]+$")]
     public string Username { get; set; }
 
     [Required, EmailAddress]
+    [Display(Name = "Email")]
     public string Email { get; set; }
 
     [Required]
+    [Display(Name = "Password")]
+    [StringLength(100, MinimumLength = 6)]
     public string Password { get; set; }
-    public string? Role { get; set; } // e.g., "Admin", "Student", "Teacher"
+    public ICollection<UserRole> UserRoles { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
