f1b57de969fe4a7e94a481c66b1ca97c671f52df
HulMarady1 <hulmarady1@gmail.com>
2026-03-27 10:43:04 +0700

    Add UserRoles and RolesPermissions collections to Role model

--- Models/Role.cs
+++ Models/Role.cs
@@ -4,7 +4,8 @@ public class Role
 {
     public int Id { get; set; }
     public string Name { get; set; }
-
+    public ICollection<UserRole> UserRoles { get; set; }
+    public ICollection<RolePermission> RolesPermissions { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
