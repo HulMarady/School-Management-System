66156a3e75204cb19d6201101aa56da39ee8919c
HulMarady1 <hulmarady1@gmail.com>
2026-03-31 16:31:43 +0700

    Add data annotations for validation in Permission model

--- Models/Permission.cs
+++ Models/Permission.cs
@@ -1,10 +1,16 @@
-﻿namespace School_Management_System.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School_Management_System.Models;
 
 public class Permission
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<RolePermission> RolesPermissions { get; set; }
+
+    [Required]
+    [Display(Name = "Permission Name")]
+    [StringLength(50, MinimumLength = 3)]
+    public string Name { get; set; }  
+    public ICollection<RolePermission> RolesPermissions { get; set; } = new List<RolePermission>();
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
