5b2b2659ad817b0f7b00dbe265b0b24faad3a5a7
HulMarady1 <hulmarady1@gmail.com>
2026-04-03 14:13:39 +0700

    Add HasPermissionAttribute for Custom Permission Handling

---
+++ Authorization/HasPermissionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace School_Management_System.Authorization;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+public class HasPermissionAttribute : AuthorizeAttribute
+{
+    public HasPermissionAttribute(string permission)
+    {
+        Policy = $"Permission:{permission}";
+    }
+}
