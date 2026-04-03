bb43d667f93cdbdb0ad0392c0b8102b8ae35bd8b
HulMarady1 <hulmarady1@gmail.com>
2026-04-03 16:32:30 +0700

    Make FirstName, LastName, and UserRoles Properties Nullable in User Model

--- Models/User.cs
+++ Models/User.cs
@@ -6,16 +6,15 @@ public class User
 {
     public int Id { get; set; }
 
-    [Required]
     [Display(Name = "First Name")]
     [StringLength(50, MinimumLength = 2)]
     [RegularExpression("^[a-zA-Z ]+$")]
-    public string FirstName { get; set; }
-    [Required]
+    public string? FirstName { get; set; }
+
     [Display(Name = "Last Name")]
     [StringLength(50, MinimumLength = 2)]
     [RegularExpression("^[a-zA-Z ]+$")]
-    public string LastName { get; set; }
+    public string? LastName { get; set; }
 
     [Required]
     [Display(Name = "Username")]
@@ -31,7 +30,7 @@ public class User
     [Display(Name = "Password")]
     [StringLength(100, MinimumLength = 6)]
     public string Password { get; set; }
-    public ICollection<UserRole> UserRoles { get; set; }
+    public ICollection<UserRole>? UserRoles { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
