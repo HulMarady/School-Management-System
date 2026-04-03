a7bc162f27f32c7895d04a01fcde67bc72e2ea2d
HulMarady1 <hulmarady1@gmail.com>
2026-04-03 14:12:04 +0700

    Add PermissionPolicyProvider for Custom Authorization Policies

---
+++ Authorization/PermissionPolicyProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace School_Management_System.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+public class PermissionPolicyProvider : DefaultAuthorizationPolicyProvider
+{
+    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options) : base(options)
+    {
+    }
+
+    public override Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+    {
+        if(policyName.StartsWith("Permission:"))
+        {
+            var permission = policyName.Substring("Permission:".Length);
+            var policy = new AuthorizationPolicyBuilder()
+                .AddRequirements(new PermissionRequirement(permission))
+                .Build();
+            return Task.FromResult<AuthorizationPolicy?>(policy);
+        }
+
+        return base.GetPolicyAsync(policyName);
+    }
+}
