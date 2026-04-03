fd7d3544ae0e74bedc259397d94ff02627f13f74
HulMarady1 <hulmarady1@gmail.com>
2026-04-03 14:12:47 +0700

    Add PermissionAuthorizationHandler for Custom Authorization Logic

---
+++ Authorization/PermissionAuthorizationHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace School_Management_System.Authorization;
+
+public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+{
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        PermissionRequirement requirement)
+    {
+        var permissions = context.User.FindAll("Permissions")
+                                    .Select(c => c.Value);
+        
+        if(permissions.Contains(requirement.Permission))
+        {
+            context.Succeed(requirement);
+        }
+        return Task.CompletedTask;
+    }
+}
