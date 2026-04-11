9263a0f2c1bd79d628f028c2918889a7f2250f0a
HulMarady1 <hulmarady1@gmail.com>
2026-04-11 22:07:17 +0700

    Add data annotations for Course properties: Name and Credits

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,8 +5,12 @@ public class Course
 {
     public int Id { get; set; } 
 
+    [Required]
+    [Display(Name = "Course Name")]
     public string Name { get; set; } 
     
+    [Required]
+    [Display(Name = "Credits")]
     public string Credits { get; set; }
 
     public int DepartmentId { get; set; }
