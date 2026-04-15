c0b1c53e5768f061d110dccd7d274f7ad232ba41
HulMarady1 <hulmarady1@gmail.com>
2026-04-15 18:31:35 +0700

    Refactor StudentController to include search functionality in Index action

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using School_Management_System.Data;
+using X.PagedList.Extensions;
 
 namespace School_Management_System.Controllers
 {
     public class StudentController : Controller
     {
-        public ActionResult Index()
+        private readonly ApplicationDbContext _applicationDbContext;
+        public StudentController(ApplicationDbContext applicationDbContext)
         {
-            return View();
+            _applicationDbContext = applicationDbContext;
+        }
+        public ActionResult Index(string? search, int page = 1, int pageSize = 10)
+        {
+            var query = _applicationDbContext.Students.AsQueryable();
+
+            if(!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(student => 
+                    student.User.FirstName.Contains(search) ||
+                    student.User.LastName.Contains(search) ||
+                    student.User.Email.Contains(search) ||
+                    student.User.Username.Contains(search) ||
+                    student.StudentId.Contains(search)
+                );
+            }
+
+            // var students = query.OrderByDescending(student => student.CreatedAt).toPagedList(page, pageSize);
         }
 
     }
