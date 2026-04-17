592cd397dd1144db40b34148f9ddac60b49bc179
HulMarady1 <hulmarady1@gmail.com>
2026-04-17 19:00:39 +0700

    Implement Index action in TeacherController for paginated teacher listing with search functionality

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,5 +1,8 @@
+using API.PagedList;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using School_Management_System.Data;
+using X.PagedList.Extensions;
 
 namespace School_Management_System.Controllers
 {
@@ -9,6 +12,27 @@ namespace School_Management_System.Controllers
         public TeacherController(ApplicationDbContext applicationDbContext)
         {
             _applicationDbContext = applicationDbContext;
+        }
+        public async Task<IActionResult> Index(string? search, int page = 1, int pageSize = 10)
+        {
+            var query = _applicationDbContext.Teachers.AsQueryable();
+
+            if(!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(teacher => 
+                    teacher.User.FirstName.Contains(search) || 
+                    teacher.User.LastName.Contains(search) || 
+                    teacher.User.Email.Contains(search) ||
+                    teacher.TeacherId.Contains(search)
+                );
+            }
+
+            var teachers = await query.OrderByDescending(teacher => teacher.CreatedAt)
+                                      .Include(teacher => teacher.User)
+                                      .Include(teacher => teacher.Department)
+                                      .ToPagedListAsync(page, pageSize);
+
+            return View();
         }
     }
 }
