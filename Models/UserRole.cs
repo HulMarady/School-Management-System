e9adfad77dc37d18fc927bf660baddba46d1c660
HulMarady1 <hulmarady1@gmail.com>
2026-04-22 16:30:54 +0700

    Remove unused model classes: Course, Department, Permission, Role, RolePermission, Student, Teacher, University, User, UserRole

--- Models/UserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace School_Management_System.Models;
-
-public class UserRole
-{
-    public int UserId { get; set; }
-    public User User { get; set;}
-    public int RoleId { get; set; }
-    public Role Role { get; set; } 
-}
