97847e0f100df9e27c7d4f46df8e6d6a7b519afe
HulMarady1 <hulmarady1@gmail.com>
2026-05-07 16:52:13 +0700

    Add CreateStudentDto for student creation data transfer

---
+++ School_Management_System.Core/DTOs/CreateStudentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace School_Management_System.Core.DTOs.Students;
+public class CreateStudentDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int DepartmentId { get; set; }
+}
